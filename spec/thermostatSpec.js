22193de5e8752f5a7b1860165dc4145e64c7b993
Peter Stevens <Peterstevens510@gmail.com>
2020-07-31 19:15:21 +0100

    updated syntax to ES6

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,109 +1,105 @@
-'use strict';
+"use strict";
 
-describe('Thermostat', function() {
-  
-  var thermostat;
+describe("Thermostat", () => {
+  let thermostat;
 
-  beforeEach(function() {
+  beforeEach(() => {
     thermostat = new Thermostat();
   });
 
-  it('starts at 20 degrees', function() {
+  it("starts at 20 degrees", () => {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
-  it('increases in temperature with increaseTemperature()', function() {
+  it("increases in temperature with increaseTemperature method", () => {
     thermostat.increaseTemperature();
-    expect(thermostat.getCurrentTemperature()).toEqual(21)
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
 
-  it('decreases in temperature with decreaseTemperature()', function() {
+  it("decreases in temperature with decreaseTemperature method", () => {
     thermostat.decreaseTemperature();
     expect(thermostat.getCurrentTemperature()).toEqual(19);
   });
 
-  it('has a minimum of 10 degrees', function() {
-    for(var i = 0; i < 11; i ++) { 
+  it("has a minimum of 10 degrees", () => {
+    for (let i = 0; i < 11; i++) {
       thermostat.decreaseTemperature();
     }
     expect(thermostat.getCurrentTemperature()).toEqual(10);
   });
-  
-  it('has power saving mode on by default',function() {
-    expect(thermostat.isPowerSavingModeOn()).toBe(true);  
+
+  it("has power saving mode on by default", () => {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
   });
 
-  it('can switch PSM off', function() {
+  it("can switch PSM off", () => {
     thermostat.switchPowerSavingModeOff();
     expect(thermostat.isPowerSavingModeOn()).toBe(false);
   });
-  
-  it('can switch PSM back on', function() {
+
+  it("can switch PSM back on", () => {
     thermostat.switchPowerSavingModeOff();
     expect(thermostat.isPowerSavingModeOn()).toBe(false);
     thermostat.switchPowerSavingModeOn();
     expect(thermostat.isPowerSavingModeOn()).toBe(true);
   });
-// normal temperature is 20, so i < 6, so as long as temperature
-// is less than 26. 
-  describe('when power saving mode is on', function() {
-    it('has a maximum temperature of 25 degrees', function(){
-      for(var i = 0; i < 6; i++) {
+  // normal temperature is 20, so i < 6, so as long as temperature
+  // is less than 26.
+  describe("when power saving mode is on", () => {
+    it("has a maximum temperature of 25 degrees", () => {
+      for (let i = 0; i < 6; i++) {
         thermostat.increaseTemperature();
-      };
+      }
       expect(thermostat.getCurrentTemperature()).toEqual(25);
     });
   });
-// normal temperature is 20, so i < 13, so as long as temperature
-// is less than 33; Since maximum temperature is 32.
-    describe('when power saving mode is off', function() {
-     it('has a maximum temperature of 32 degrees', function() {
+  // normal temperature is 20, so i < 13, so as long as temperature
+  // is less than 33; Since maximum temperature is 32.
+  describe("when power saving mode is off", () => {
+    it("has a maximum temperature of 32 degrees", () => {
       thermostat.switchPowerSavingModeOff();
-      for (var i = 0; i < 13; i++) { 
+      for (let i = 0; i < 13; i++) {
         thermostat.increaseTemperature();
       }
       expect(thermostat.getCurrentTemperature()).toEqual(32);
     });
 
-    it('can be reset to the default temperature', function() {
-      for (var i = 0; i < 6; i++) {
+    it("can be reset to the default temperature", () => {
+      for (let i = 0; i < 6; i++) {
         thermostat.increaseTemperature();
-      };
+      }
       thermostat.resetTemperature();
       expect(thermostat.getCurrentTemperature()).toEqual(20);
     });
 
-    describe('displaying usage levels', function(){
-      describe('when the temperature is below 18 degrees', function() {
-        it('it is considered low-usage', function() {
-          for (var i = 0; i < 3; i++) {
+    describe("displaying usage levels", () => {
+      describe("when the temperature is below 18 degrees", () => {
+        it("it is considered low-usage", () => {
+          for (let i = 0; i < 3; i++) {
             thermostat.decreaseTemperature();
-          };
-          expect(thermostat.energyUsage()).toEqual('low-usage');
+          }
+          expect(thermostat.energyUsage()).toEqual("low-usage");
         });
       });
     });
 
-      describe('when the temperature is between 18 and 25 degrees', function() {
-        it('it is considered medium-usage', function() {
-         expect(thermostat.energyUsage()).toEqual('medium-usage'); 
-        });
+    describe("when the temperature is between 18 and 25 degrees", () => {
+      it("it is considered medium-usage", () => {
+        expect(thermostat.energyUsage()).toEqual("medium-usage");
       });
     });
-      
-      describe('when the temperature is anything else', function() {
-        it('it is considered high-usage', function() {
-          thermostat.powerSavingMode = false;
-          for (var i = 0; i < 6; i++) {
-            thermostat.increaseTemperature();
-          };
-          expect(thermostat.energyUsage()).toEqual('high-usage');
-        });
-      });
+  });
+
+  describe("when the temperature is anything else", () => {
+    it("it is considered high-usage", () => {
+      thermostat.powerSavingMode = false;
+      for (let i = 0; i < 6; i++) {
+        thermostat.increaseTemperature();
+      }
+      expect(thermostat.energyUsage()).toEqual("high-usage");
     });
-  
-
-
+  });
+});
 
 // While i is less than 11, increment the value of i by 1.
 // Set i = 0, condition is += 1.
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,87 +1,82 @@
-'use strict';
+"use strict";
 // Object constructor function, here we're adding properties to our
 // Object constructor function.
-function Thermostat() {
-  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
-  this.DEFAULT_TEMPERATURE = 20;
-  this.temperature = this.DEFAULT_TEMPERATURE;
-  this.MAX_LIMIT_PSM_ON = 25
-  this.MAX_LIMIT_PSM_OFF = 32
-  this.powerSavingMode = true;
-  this.MINIMUM_TEMPERATURE = 10;
-  this.temperature = 20;
-}; 
-
-Thermostat.prototype.getCurrentTemperature = function() {
-  return this.temperature;
-};
-
-Thermostat.prototype.increaseTemperature = function() {
-  this.temperature ++;
-};
-// if method? if after calling the decreaseTemperature method the thermostat 
-// matches the minimum temperature then return the min temp of 10,
-// else increment the temperature by -1.
-Thermostat.prototype.decreaseTemperature = function() {
-  if (this.isMinimumTemperature()) {
-    return;
+class Thermostat {
+  constructor() {
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+    this.DEFAULT_TEMPERATURE = 20;
+    this.temperature = this.DEFAULT_TEMPERATURE;
+    this.MAX_LIMIT_PSM_ON = 25;
+    this.MAX_LIMIT_PSM_OFF = 32;
+    this.powerSavingMode = true;
+    this.MINIMUM_TEMPERATURE = 10;
+    this.temperature = 20;
   }
-  this.temperature --;
-};
 
-Thermostat.prototype.isMinimumTemperature = function() {
-  return this.temperature === this.MINIMUM_TEMPERATURE;
-};
+  getCurrentTemperature() {
+    return this.temperature;
+  }
 
-Thermostat.prototype.isPowerSavingModeOn = function(){
-  return this.powerSavingMode === true; 
-};
-// Setting powerSavingMode equal to false suggesting we're now
-// turning power saving mode off.
-Thermostat.prototype.switchPowerSavingModeOff = function() {
-  this.powerSavingMode = false;
-};
+  increaseTemperature() {
+    this.temperature++;
+  }
+  // if method? if after calling the decreaseTemperature method the thermostat
+  // matches the minimum temperature then return the min temp of 10,
+  // else increment the temperature by -1.
+  decreaseTemperature() {
+    if (this.isMinimumTemperature()) {
+      return;
+    }
+    this.temperature--;
+  }
 
-Thermostat.prototype.switchPowerSavingModeOn = function() {
-  this.powerSavingMode = true; 
-};
-// If our current temperature is already set to the maximum limit, 
-// we do nothing - otherwise we turn it increaseTemperature. 
-Thermostat.prototype.increaseTemperature = function() {
-  if(this.isMaximumTemperature()){
-    return;
-  };
-  this.temperature ++;
-};
-// This is saying if power saving mode is off then return 
-// the max temperature of 32 degrees, otherwise if power saving
-// mode is ON then return the lower temperature to 25 degrees.   
-Thermostat.prototype.isMaximumTemperature = function() {
-  if(this.isPowerSavingModeOn() === false) {
-    return this.temperature === this.MAX_LIMIT_PSM_OFF;
-  };
-  return this.temperature === this.MAX_LIMIT_PSM_ON;
-};
-// Setting the temperature back to the default setting of 20.
-Thermostat.prototype.resetTemperature = function() {
-  this.temperature = this.DEFAULT_TEMPERATURE
-};
+  isMinimumTemperature() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
 
-Thermostat.prototype.energyUsage = function() {
-  if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
-    return 'low-usage';
-  };
-  if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT & this.temperature <= this.MAX_LIMIT_PSM_ON) {
-    return 'medium-usage';
-  };
-  return 'high-usage';
-};
+  isPowerSavingModeOn() {
+    return this.powerSavingMode === true;
+  }
+  // Setting powerSavingMode equal to false suggesting we're now
+  // turning power saving mode off.
+  switchPowerSavingModeOff() {
+    this.powerSavingMode = false;
+  }
 
+  switchPowerSavingModeOn() {
+    this.powerSavingMode = true;
+  }
+  // If our current temperature is already set to the maximum limit,
+  // we do nothing - otherwise we turn it increaseTemperature.
+  increaseTemperature() {
+    if (this.isMaximumTemperature()) {
+      return;
+    }
+    this.temperature++;
+  }
+  // This is saying if power saving mode is off then return
+  // the max temperature of 32 degrees, otherwise if power saving
+  // mode is ON then return the lower temperature to 25 degrees.
+  isMaximumTemperature() {
+    if (this.isPowerSavingModeOn() === false) {
+      return this.temperature === this.MAX_LIMIT_PSM_OFF;
+    }
+    return this.temperature === this.MAX_LIMIT_PSM_ON;
+  }
+  // Setting the temperature back to the default setting of 20.
+  resetTemperature() {
+    this.temperature = this.DEFAULT_TEMPERATURE;
+  }
 
-// I changed up to increaseTemperature
-// I also changed down to decreaseTemperature
-
-
-
-
-
+  energyUsage() {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return "low-usage";
+    } else if (
+      (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT) &
+      (this.temperature <= this.MAX_LIMIT_PSM_ON)
+    ) {
+      return "medium-usage";
+    }
+    return "high-usage";
+  }
+}
